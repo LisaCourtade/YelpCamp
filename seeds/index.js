0ef219f0995b04063bcf24b103c88e7e6adda6af
LisaCourtade <lisa.courtade@hotmail.fr>
2023-01-20 12:46:55 +0200

    authorisations for campgrounds and reviews POST PUT DEL

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '63c8fbd78e9f5972dd6b8228',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://source.unsplash.com/random/300x300?camping,${i}`,
